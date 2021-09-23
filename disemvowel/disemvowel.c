ac8b119d6dcba4efdabb2f1d2582e95a5a9c4325
Richard Lussier <lussi036@morris.umn.edu>
2021-09-23 11:44:47 -0500

    Implement disemvowel method in disemvowel.c
    We implemented the disemvowel method, whch takes a string and returns the string with all vowels removed. There are current memory leaks inside of the disemvowel_test file, which will be addressed in a later commit.
    
    Co-authored-by: ErikRauer <rauer007@morris.umn.edu>

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,16 +1,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <string.h>
 
 #include "disemvowel.h"
-
-char *disemvowel(char *str) {
-
-  int i;
-  for(i = 0; i < strlen(str); i++) {
-  }
-  return (char*) "";
-}
 
 /*
  * This method checks whether a given charcter is a vowel (upper or lowercase)
@@ -18,10 +11,36 @@ char *disemvowel(char *str) {
 */
 bool isVowel(char letter) {
   char lowercaseLetter = tolower(letter);
-  if(lowercaseLetter == "a" || lowercaseLetter == "e"
-	|| lowercaseLetter == "i"  || lowercaseLetter == "o" || lowercaseLetter == "u") {
+  if(lowercaseLetter == 'a' || lowercaseLetter == 'e'
+	|| lowercaseLetter == 'i'  || lowercaseLetter == 'o' || lowercaseLetter == 'u') {
     return true;
   } else {
     return false;
   }
+}
+
+
+char *disemvowel(char *str) {
+  
+  int len = strlen(str);
+  int i;
+  int numOfConsonants = 0;
+  for(i = 0; i < len; i++) {
+    if(!isVowel(str[i])) {
+      numOfConsonants++;
+    }
+  }
+
+  char *disemvoweledString = (char*) calloc(numOfConsonants + 1, sizeof(char));
+
+  int j;
+  int currentIndex = 0;
+  for(j = 0; j < len; j++) {
+    if(!isVowel(str[j])) {
+      disemvoweledString[currentIndex] = str[j];
+      currentIndex++;
+    }
+  }
+  disemvoweledString[numOfConsonants] = '\0';
+  return disemvoweledString;
 }
