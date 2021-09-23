8a4f71e05ae7d4b29a43032b32a76b12b267dff2
Richard Lussier <lussi036@morris.umn.edu>
2021-09-23 11:13:56 -0500

    Implement isVowel method and fix memory leak in main
    Implemented a method inside of disemvowel.c that checks whether a given letter is a vowel. Additionally, we fixed a memory leak inside of main.c concerning a call to disemvowel not being freed afterwards.
    
    Co-authored-by: ErikRauer <rauer007@morris.umn.edu>

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,27 @@
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
+
+  int i;
+  for(i = 0; i < strlen(str); i++) {
+  }
   return (char*) "";
+}
+
+/*
+ * This method checks whether a given charcter is a vowel (upper or lowercase)
+ * and returns true if it is, false otherwise.
+*/
+bool isVowel(char letter) {
+  char lowercaseLetter = tolower(letter);
+  if(lowercaseLetter == "a" || lowercaseLetter == "e"
+	|| lowercaseLetter == "i"  || lowercaseLetter == "o" || lowercaseLetter == "u") {
+    return true;
+  } else {
+    return false;
+  }
 }
