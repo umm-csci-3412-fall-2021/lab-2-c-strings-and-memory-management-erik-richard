dbd8005e84d07d7d67eb2cda81e834ef4326217d
Richard Lussier <lussi036@morris.umn.edu>
2021-09-28 10:33:04 -0500

    Add comments to disemvowel.c
    Added additional comments inside of the disemvowel.c file for better user readability.
    
    Co-authored-by: ErikRauer <rauer007@morris.umn.edu>

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -8,7 +8,7 @@
 /*
  * This method checks whether a given charcter is a vowel (upper or lowercase)
  * and returns true if it is, false otherwise.
-*/
+ */
 bool isVowel(char letter) {
   char lowercaseLetter = tolower(letter);
   if(lowercaseLetter == 'a' || lowercaseLetter == 'e'
@@ -19,12 +19,17 @@ bool isVowel(char letter) {
   }
 }
 
-
+/*
+ * This method takes a string and will return a new string with
+ * all of the vowels removed from it.
+ */
 char *disemvowel(char *str) {
-  
+ 
   int len = strlen(str);
   int i;
   int numOfConsonants = 0;
+  //counts the number of consonants (non-vowels) so we
+  //know how much memory to allocate to our solution.
   for(i = 0; i < len; i++) {
     if(!isVowel(str[i])) {
       numOfConsonants++;
@@ -35,12 +40,15 @@ char *disemvowel(char *str) {
 
   int j;
   int currentIndex = 0;
+  //checks whether each letter is a vowel. If it isn't,
+  //we will add it to the solution.
   for(j = 0; j < len; j++) {
     if(!isVowel(str[j])) {
       disemvoweledString[currentIndex] = str[j];
       currentIndex++;
     }
   }
+  //adds the termination character to the end of our solution.
   disemvoweledString[numOfConsonants] = '\0';
   return disemvoweledString;
 }
